fb3ab08a0f117d9e34e8359c210cd413214439c7
wellington <wellington281482@gmail.com>
2026-06-28 01:08:09 -0300

    progressão do cadastro_alunos

--- 2-Básico/2.7-Dicionários/cadastro_alunos.py
+++ 2-Básico/2.7-Dicionários/cadastro_alunos.py
@@ -7,24 +7,14 @@
 # Lista principal responsável por armazenar todos os alunos
 # cadastrados no sistema.
 alunos = []
+MEDIA_MINIMA = 7.0
 
 # =========================================================
 # FUNÇÕES DO SISTEMA
 # =========================================================
 
-def adicionar_aluno():
-    """
-    Realiza o cadastro de um novo aluno.
-
-    Processo:
-        1. Solicita o nome do aluno.
-        2. Solicita as quatro notas.
-        3. Cria um dicionário contendo os dados.
-        4. Adiciona o cadastro na lista global de alunos.
-
-    O usuário pode digitar 'q' para sair do cadastro.
-    """
-
+# Realiza o cadastro de um novo aluno.
+def adicionar_aluno():  
     while True:
         # Cria um novo dicionário para cada aluno cadastrado.
         # Isso evita sobrescrever registros anteriores.
@@ -36,10 +26,10 @@ def adicionar_aluno():
         print("    *** Digite 'q' pra sair... ***\n")
 
         # Entrada do nome do aluno
-        nome = input("Informe o nome do aluno: ")
+        nome = input("Informe o nome do aluno: ").strip()
 
         # Permite sair do cadastro
-        if nome == 'q' or nome == 'Q': break
+        if nome.lower() == 'q': break
 
         # Entrada das notas convertidas para número decimal
         nota_1 = float(input("Informe a primeira nota: "))
@@ -47,25 +37,28 @@ def adicionar_aluno():
         nota_3 = float(input("Informe a terceira nota: "))
         nota_4 = float(input("Informe a quarta nota: "))
 
+        # Faz o cálculo da média
+        media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+        # Faz a verificação, com base na média, se o aluno está aprovado ou não
+        status = 'Aprovado' if  media >= MEDIA_MINIMA else 'Reprovado'
+
         # Armazena os dados do aluno no dicionário
-        cadastro_aluno['nome'] = nome
-        cadastro_aluno['nota_1'] = nota_1
-        cadastro_aluno['nota_2'] = nota_2
-        cadastro_aluno['nota_3'] = nota_3
-        cadastro_aluno['nota_4'] = nota_4
+        cadastro_aluno = {
+            'nome': nome,
+            'nota_1': nota_1,
+            'nota_2': nota_2,
+            'nota_3': nota_3,
+            'nota_4': nota_4,
+            'media': media,
+            'status': status
+        }
 
         # Adiciona o aluno na lista principal
         alunos.append(cadastro_aluno)
  
-
+#  Exibe todos os alunos cadastrados no sistema.
 def listar_alunos():
-    """
-    Exibe todos os alunos cadastrados no sistema.
-
-    Caso a lista esteja vazia, informa ao usuário
-    que não existem registros.
-    """
-
     print("\n" + "=" * 40)
     print("             LISTA DE ALUNOS")
     print("=" * 40)
@@ -76,35 +69,25 @@ def listar_alunos():
     else:
         # Percorre cada aluno armazenado na lista
         for aluno in alunos:
-            print(f"Aluno : {aluno['nome']}")
-            print(f"Nota 1: {aluno['nota_1']}")
-            print(f"Nota 2: {aluno['nota_2']}")
-            print(f"Nota 3: {aluno['nota_3']}")
-            print(f"Nota 4: {aluno['nota_4']}")
-            
+            print(f"\nAluno...: {aluno['nome']}")
+            print(f"Nota 1..: {aluno['nota_1']}")
+            print(f"Nota 2..: {aluno['nota_2']}")
+            print(f"Nota 3..: {aluno['nota_3']}")
+            print(f"Nota 4..: {aluno['nota_4']}")
+            print(f"Média...: {aluno['media']}")
+            print(f"Situação: {aluno['status']}")
             print("-" * 40)
 
     input("\nPressione ENTER para voltar ao menu")
 
-def pesquisar_aluno():
-    """
-    Pesquisa um aluno pelo nome informado pelo usuário.
-
-    Funcionamento:
-        - Percorre a lista de alunos.
-        - Compara o nome pesquisado.
-        - Exibe os dados caso encontre.
-        - Informa caso o aluno não exista.
-
-    A comparação ignora diferença entre letras
-    maiúsculas e minúsculas.
-    """
-     
+# Pesquisa um aluno pelo nome informado pelo usuário.
+def pesquisar_aluno():   
     print("\n" + "=" * 40)
     print("             PESQUISAR ALUNO")
     print("=" * 40)
 
-    busca_aluno = input("Infome o aluno que deseja pesquisar: ")
+    # Recebe o nome do aluno a ser procurado
+    busca_aluno = input("Infome o aluno que deseja pesquisar: ").strip()
     
     if not alunos:
         print("    * Nenhum cadastro encontrado. *")
@@ -115,11 +98,12 @@ def pesquisar_aluno():
         for aluno in alunos:
             # Compara os nomes ignorando maiúsculas/minúsculas
             if aluno['nome'].lower() == busca_aluno.lower():
-                print(f"\nAluno : {aluno['nome']}")
-                print(f"Nota 1: {aluno['nota_1']}")
-                print(f"Nota 2: {aluno['nota_2']}")
-                print(f"Nota 3: {aluno['nota_3']}")
-                print(f"Nota 4: {aluno['nota_4']}")
+                print(f"\nAluno.....: {aluno['nome']}")
+                print(f"Nota 1....: {aluno['nota_1']}")
+                print(f"Nota 2....: {aluno['nota_2']}")
+                print(f"Nota 3....: {aluno['nota_3']}")
+                print(f"Nota 4....: {aluno['nota_4']}")
+                print(f"Situação..: {aluno['status']}")
 
                 encontrado = True
 
@@ -129,42 +113,92 @@ def pesquisar_aluno():
 
     input("\nPressione ENTER para voltar ao menu")
 
+# Remove um aluno cadastrado.
 def remover_aluno():
-    """
-    Remove um aluno cadastrado.
-
-    Função reservada para implementação futura.
-    """
-
     print("\n" + "=" * 40)
     print("             REMOVER ALUNO")
     print("=" * 40)
-    return 0
+    
+    aluno_remover = input("Infome o aluno que deseja pesquisar: ").strip()
 
+    encontrado = False
+
+    for aluno in alunos:
+        if aluno['nome'] == aluno_remover:
+            alunos.remove(aluno)
+            print(f"\n* Aluno '{aluno['nome']}' removido com sucesso! *")
+            encontrado = True
+            break
+    
+    if not encontrado:
+        print("\n      * Aluno não encontrado. *")
+
+    input("\nPressione ENTER para voltar ao menu")
+
+# Permite alterar informações de um aluno.
 def alterar_aluno():
-    """
-    Permite alterar informações de um aluno.
-
-    Função reservada para implementação futura.
-    """
-
     print("\n" + "=" * 40)
     print("             ALTERAR ALUNO")
     print("=" * 40)
-    return 0
+    
+    aluno_alterar = input("Infome o aluno que deseja alterar: ").strip()
 
+    for aluno in alunos:
+        if aluno['nome'] == aluno_alterar:
+            n_nome = input(f"Informe o novo nome do aluno [{aluno['nome']}]: ").split()
+            n_nota_1 = float(input(f"Infome a nova nota 1 [{aluno['nota_1']}]: "))
+            n_nota_2 = float(input(f"Infome a nova nota 2 [{aluno['nota_2']}]: "))
+            n_nota_3 = float(input(f"Infome a nova nota 3 [{aluno['nota_3']}]: "))
+            n_nota_4 = float(input(f"Infome a nova nota 4 [{aluno['nota_4']}]: "))
+    
+            # Faz o cálculo da média
+            media = (n_nota_1 + n_nota_2 + n_nota_3 + n_nota_4) / 4
+
+            # Faz a verificação, com base na média, se o aluno está aprovado ou não
+            status = 'Aprovado' if  media >= MEDIA_MINIMA else 'Reprovado'
+
+            aluno = {
+                'nome': n_nome,
+                'nota_1': n_nota_1,
+                'nota_2': n_nota_2,
+                'nota_3': n_nota_3,
+                'nota_4': n_nota_4,
+                'media': media,
+                'status': status
+            }
+        
+    alunos.append(aluno)
+
+
+# Exibe alunos que atingiram média mínima.
 def exibir_alunos_aprovados():
-    """
-    Exibe alunos que atingiram média mínima.
-
-    Função reservada para implementação futura.
-    """
-
     print("\n" + "=" * 40)
     print("             EXIBIR APROVADOS")
     print("=" * 40)
-    return 0
+    
+    if not alunos:
+        print("    * Nenhum cadastro encontrado. *")
+    else:
+        encontrado = False
 
+        # Percorre todos os alunos cadastrados
+        for aluno in alunos:
+            if aluno['status'] == 'Aprovado':
+                print(f"Aluno.....: {aluno['nome']}")
+                print(f"Nota 1....: {aluno['nota_1']}")
+                print(f"Nota 2....: {aluno['nota_2']}")
+                print(f"Nota 3....: {aluno['nota_3']}")
+                print(f"Nota 4....: {aluno['nota_4']}")
+                print(f"Média.....: {aluno['media']}")
+                print(f"Situação..: {aluno['status']}")
+                print("-" * 40)
+
+                encontrado = True
+
+        if not encontrado:
+            print("\n    * Nenhum aluno aprovado encontrado")
+
+    input("\nPressione ENTER para voltar ao menu")  
 
 # =========================================================
 # MENU PRINCIPAL (INTERFACE DO USUÁRIO)
@@ -204,10 +238,11 @@ while True:
         
     elif opcao == '6':
         exibir_alunos_aprovados()
-        
+
     elif opcao == '0':
         print("\nSaindo...")
+        break
         
     else:
-        print("\nOpção inválida!")
-        break
+        print("\nOpção inválida! Tente novamente.") 
+        input("\nPressione ENTER para voltar ao menu") 
--- 2-Básico/2.7-Dicionários/dicionario.py
+++ 2-Básico/2.7-Dicionários/dicionario.py
@@ -22,57 +22,66 @@
 
 # # print("\n") # Imprime uma linha em branco para melhor formatação.
 
-nome = input("Informe o nome: ")
-sobrenome = input("Informe o sobrenome: ")
-idade = input("Informe a idade: ")
-cidade = input("Informe a cidade: ")
+# nome = input("Informe o nome: ")
+# sobrenome = input("Informe o sobrenome: ")
+# idade = input("Informe a idade: ")
+# cidade = input("Informe a cidade: ")
 
-info = {
-    'nome': nome,
-    'sobrenome': sobrenome,
-    'idade': idade,
-    'cidade': cidade
-}
+# info = {
+#     'nome': nome,
+#     'sobrenome': sobrenome,
+#     'idade': idade,
+#     'cidade': cidade
+# }
 
-print(f"Antes: {info}")
+# print(f"Antes: {info}")
 
-# #Assim, trata também espaços: Entrada: "     " não deve ser considerado um nome válido.
+# # #Assim, trata também espaços: Entrada: "     " não deve ser considerado um nome válido.
 
-# Recebe os dados
-n_nome = input("Informe o novo nome: ").strip()
-n_sobrenome = input("Informe o novo sobrenome: ").strip()
-n_idade = input("Informe a nova idade: ").strip()
-n_cidade = input("Informe a nova cidade: ").strip()
+# # Recebe os dados
+# n_nome = input("Informe o novo nome: ").strip()
+# n_sobrenome = input("Informe o novo sobrenome: ").strip()
+# n_idade = input("Informe a nova idade: ").strip()
+# n_cidade = input("Informe a nova cidade: ").strip()
 
-# Atualiza, caso não for vazio.
-if n_nome: info['nome'] = n_nome
-if n_sobrenome: info['nome'] = n_sobrenome
-if n_idade: info['idade'] = n_idade
-if n_cidade: info['cidade'] = n_cidade
+# # Atualiza, caso não for vazio.
+# if n_nome: info['nome'] = n_nome
+# if n_sobrenome: info['nome'] = n_sobrenome
+# if n_idade: info['idade'] = n_idade
+# if n_cidade: info['cidade'] = n_cidade
 
-# Exibe os novos dados em uma única linha.
-print(f"Depois: {info}")
+# # Exibe os novos dados em uma única linha.
+# print(f"Depois: {info}")
 
-# Exibe os novos dados em várias linhas.
-print(f"Nome: {info['nome']}")
-print(f"Sobrenome: {info['sobrenome']}")
-print(f"Idade: {info['idade']}")
-print(f"Cidade: {info['cidade']}")
+# # Exibe os novos dados em várias linhas.
+# print(f"Nome: {info['nome']}")
+# print(f"Sobrenome: {info['sobrenome']}")
+# print(f"Idade: {info['idade']}")
+# print(f"Cidade: {info['cidade']}")
 
-# print(bool(""))
-# print(bool("joão"))
+# # print(bool(""))
+# # print(bool("joão"))
 
-# o 'pop' remove e retorna quem foi removido.
-removido = info.pop('nome')
+# # o 'pop' remove e retorna quem foi removido.
+# removido = info.pop('nome')
 
-print(f"{removido} foi removido com sucesso!")
+# print(f"{removido} foi removido com sucesso!")
 
-# o 'del' apenas remove sem retornar quem foi removido.
-del info['idade']
+# # o 'del' apenas remove sem retornar quem foi removido.
+# del info['idade']
 
-print(info)
+# print(info)
 
-# adicionando mais um elemento ao dicionário
-info['pais'] = 'Brasil'
+# # adicionando mais um elemento ao dicionário
+# info['pais'] = 'Brasil'
 
-print(info)
+# print(info)
+
+lista = [{'a': 1, 'b': 2, 'c':3}, {'A': 1, 'B': 2, 'C': 3}]
+
+#print(lista[0]['a'])
+
+for dic in lista:
+    if dic['a'] == 1:
+        lista.remove(dic)
+print(lista)
