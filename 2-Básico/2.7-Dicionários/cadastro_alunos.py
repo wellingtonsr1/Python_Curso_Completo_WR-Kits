ef226b9b0203fc25613050f6d5ba33dbbcf2b83f
wellingtonsr1 <wellington281482@gmail.com>
2026-06-30 11:01:18 -0300

    Correção do comtentário na funçaõ main

--- 2-Básico/2.7-Dicionários/cadastro_alunos.py
+++ 2-Básico/2.7-Dicionários/cadastro_alunos.py
@@ -361,6 +361,6 @@ def main():
             # Aguarda o usuário pressionar ENTER antes de retornar ao menu
             input("\nPressione ENTER para voltar ao menu.") 
 
-# Incia o programa
+# Inicia o programa
 if __name__ == "__main__":
     main()
